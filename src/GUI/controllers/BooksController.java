429a531c63eb3e00840e556ad8a16e5d8142128c
Pawel Pabianczyk <pawel.pabianczyk1999@gmail.com>
2021-01-04 14:30:36 +0100

    Add searching to books controller

--- src/GUI/controllers/BooksController.java
+++ src/GUI/controllers/BooksController.java
@@ -5,9 +5,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import models.Book;
 
 import java.io.IOException;
@@ -39,27 +42,43 @@ public class BooksController implements Initializable {
     @FXML
     private TableColumn<Book, String> status;
 
-    //    Node node = (Node) mouseEvent.getSource();
-    //    Stage stage = (Stage) node.getScene().getWindow();
-    //    user = (User) stage.getUserData();
+    @FXML
+    private ChoiceBox<String> choiceCategory;
+
+    @FXML
+    private TextField tfPhrase;
+
+    private ObservableList<String> categories = FXCollections.observableArrayList("All","Title", "Author", "Genre");
+
+    private ObservableList<Book> data;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        final ObservableList<Book> data = FXCollections.observableArrayList(getUsersBooks());
-        title.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
-        author.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
-        genre.setCellValueFactory(new PropertyValueFactory<Book, String>("genre"));
-        publishDate.setCellValueFactory(new PropertyValueFactory<Book, LocalDate>("publishDate"));
-        status.setCellValueFactory(new PropertyValueFactory<Book, String>("status"));
-        booksTable.setItems(data);
+        choiceCategory.setValue("All");
+        choiceCategory.setItems(categories);
+        data = FXCollections.observableArrayList(getUsersBooks("All"));
+        showData(data);
     }
 
-    private ArrayList<Book> getUsersBooks(){
-
+    private ArrayList<Book> getUsersBooks(String category){
         try {
             Socket socket = new Socket("localhost", 4444);
             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-            outputStream.writeObject("GET user books");
+
+            switch (category){
+                case "All":
+                    outputStream.writeObject("GET user books");
+                    break;
+                case "Title":
+                    outputStream.writeObject("GET user books with title");
+                    break;
+                case "Author":
+                    outputStream.writeObject("GET user books with author");
+                    break;
+                case "Genre":
+                    outputStream.writeObject("GET user books with genre");
+                    break;
+            }
             outputStream.writeObject(UserHolder.getInstance().getUser().getUsername());
 
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
@@ -72,5 +91,23 @@ public class BooksController implements Initializable {
         }
 
         return null;
+    }
+
+    public void search(MouseEvent mouseEvent) {
+        String category = choiceCategory.getValue();
+        String phrase = tfPhrase.getText();
+        data = FXCollections.observableArrayList(getUsersBooks(category));
+        showData(data);
+        choiceCategory.setValue("All");
+        tfPhrase.setText("Phrase");
+    }
+
+    private void showData(ObservableList<Book> data){
+        title.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+        author.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
+        genre.setCellValueFactory(new PropertyValueFactory<Book, String>("genre"));
+        publishDate.setCellValueFactory(new PropertyValueFactory<Book, LocalDate>("publishDate"));
+        status.setCellValueFactory(new PropertyValueFactory<Book, String>("status"));
+        booksTable.setItems(data);
     }
 }
